86f8841b9ef41a6999e98813f575750191f4b2fa
ozgursertel <53472254+ozgursertel@users.noreply.github.com>
2022-06-13 00:22:50 +0300

    Camera Movement

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+        transform.rotation = target.rotation;
+    }
+}
