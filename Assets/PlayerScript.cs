8e4a69f788f84fcd71073708fb9ffd99b2f37f62
ozgursertel <sertel.ozgur@gmail.com>
2022-06-13 14:42:57 +0300

    Player Movement Platform to Platform

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,37 +2,66 @@ using Dreamteck.Splines;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class PlayerScript : MonoBehaviour
 {
+    private GameObject[] playerPositions;
+    private int positionCounter = 0;
+    private bool checkEnemy;
+
+    public float positionChangeTime;
     public float radius;
-    SplineFollower splineFollower;
     // Start is called before the first frame update
     void Start()
     {
-        splineFollower = GetComponent<SplineFollower>();
-
+        playerPositions = FindObsWithTag("PlayerPosition");
+        checkEnemy = true;
+        foreach(GameObject obj in playerPositions)
+        {
+            Debug.Log("Player Position pos" + obj.transform.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!Physics.CheckSphere(transform.position, radius, 1 << 6))
+        if(!Physics.CheckSphere(transform.position, radius, 1 << 6) && checkEnemy)
         {
-            if(splineFollower != null)
+            checkEnemy = false;
+            positionCounter++;
+            if(positionCounter < playerPositions.Length)
             {
-               splineFollower.enabled = true;
-
+                StartCoroutine(MoveNextPosition(playerPositions[positionCounter].transform.position));
+            }
+            else
+            {
+                Debug.Log("Level Ended");
             }
         }
+
     }
 
-   void MovingEnd()
+    IEnumerator MoveNextPosition(Vector3 playerPos)
     {
-        Debug.Log("Move Ended");
-        splineFollower.enabled = false;
-        splineFollower.spline = GameObject.Find("Spline 1").GetComponent<SplineComputer>();
-        splineFollower.Restart();
+        Debug.Log("Moving Position");
+        transform.DOMove(playerPositions[positionCounter].transform.position, positionChangeTime);
+        transform.DORotate(playerPositions[positionCounter].transform.rotation.eulerAngles, positionChangeTime);
+        yield return new WaitForSeconds(positionChangeTime);
+        checkEnemy = true;
+    }
+
+    GameObject[] FindObsWithTag(string tag)
+    {
+        GameObject[] foundObs = GameObject.FindGameObjectsWithTag(tag);
+        Array.Sort(foundObs, CompareObNames);
+        return foundObs;
+    }
+
+    int CompareObNames(GameObject x, GameObject y)
+    {
+        return x.name.CompareTo(y.name);
     }
 
 }
