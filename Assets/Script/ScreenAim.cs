bdb5b52466bd16e3e2c93492277f8e59a4b6ec54
ozgursertel <sertel.ozgur@gmail.com>
2022-06-15 21:11:41 +0300

    HoldAndDrag

--- Assets/Script/ScreenAim.cs
+++ Assets/Script/ScreenAim.cs
@@ -7,7 +7,10 @@ public class ScreenAim : MonoBehaviour
 
     public float force;
     GameObject enemy;
-
+    private bool dragging;
+    private float dist;
+    private Vector3 offset;
+    private Transform toDrag;
     
 
     // Start is called before the first frame update
@@ -19,7 +22,52 @@ public class ScreenAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HitEnemy();
+
+        Vector3 v3;
+        if(Input.touchCount != 1)
+        {
+            dragging = false;
+            return;
+        }
+
+        Touch touch = Input.touches[0];
+        Vector3 pos = touch.position;
+
+
+        if(touch.phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    enemy = hit.collider.gameObject;
+                    enemy.GetComponent<EnemyScript>().HitFromTaser(ray, force, hit);
+                    toDrag = hit.transform;
+                    dist = hit.transform.position.z - Camera.main.transform.position.z;
+                    v3 = new Vector3(pos.x, pos.y, dist);
+                    v3 = Camera.main.ScreenToWorldPoint(v3);
+                    offset = toDrag.position - v3;
+                    dragging = true;
+
+                }
+            }
+        }
+
+        if(dragging && touch.phase == TouchPhase.Moved)
+        {
+            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            toDrag.position = v3 + offset;
+        }
+
+        if(dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
+        {
+            dragging = false;
+           
+
+        }
     }
 
     void HitEnemy()
